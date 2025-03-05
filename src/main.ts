f6ab4b7149fd32064b309517263c523b479fb95e
mulf <mail@mulf.wtf>
2025-03-05 15:47:29 -0600

    init

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import 'dotenv/config';
+import { createWalletClient, http, type Chain } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
+import { treasureTopaz, treasure } from 'viem/chains';
+
+if (!process.env.TESTNET_WALLET) {
+    throw new Error('Wallet private key missing');
+}
+
+const account = privateKeyToAccount(process.env.TESTNET_WALLET as `0x${string}`);
+
+const args = process.argv.slice(2);
+
+const networkArg = args[0];
+let network: Chain | null = null;
+
+if (!networkArg) {
+    throw new Error('network arg must be passed in\n npm run start -- {network}');
+}
+
+switch (networkArg) {
+    case 'testnet':
+        network = treasureTopaz;
+        break;
+    case 'mainnet':
+        network = treasure;
+        break;
+    default:
+        throw new Error('Invalid Arg');
+}
+
+try {
+    const client = createWalletClient({
+        account,
+        chain: network,
+        transport: http(),
+    });
+
+    const hash = await client.sendTransaction({
+        account,
+        to: account.address,
+        value: 0n,
+    });
+
+    console.log('Tx sent:', hash);
+} catch (e) {
+    if (e instanceof Error) {
+        console.error(e.message);
+    }
+
+    console.error({ e });
+}
